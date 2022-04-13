5b5efeedeb7db728bec45a77830e3dee1c910604
Jennifer Manwell <manwell.fortier@gmail.com>
2022-04-13 12:57:43 -0400

    Snake Game - Successfully connect to server

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require('net');
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243', // change to IP address of computer or ngrok host if tunneling
+    port:  50541// or change to the ngrok port if tunneling
+  });
+  
+  conn.setEncoding('utf8'); // interpret data as text
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = {connect};
