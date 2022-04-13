c45669c1b7db1443a9631828ed196e9ef7b0bd11
Jennifer Manwell <manwell.fortier@gmail.com>
2022-04-13 13:00:52 -0400

    Snake Game - Set up connection module

--- client.js
+++ client.js
@@ -15,7 +15,4 @@ const connect = function () {
   return conn;
 };
 
-console.log("Connecting ...");
-connect();
-
 module.exports = {connect};
--- play.js
+++ play.js
@@ -1,2 +1,5 @@
-const {connect} = require('./connect');
+const {connect} = require('./client');
+
+console.log("Connecting ...");
+connect();
 
