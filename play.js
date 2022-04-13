2925d96d7253f82e18aec8f8220545aa5447b381
Jennifer Manwell <manwell.fortier@gmail.com>
2022-04-13 13:20:11 -0400

    Snake Game - Added code to automate snake movements

--- play.js
+++ play.js
@@ -7,4 +7,18 @@ const client = connect();
 //Create a snake!
 client.on('connect', () => {
   client.write("Name: JAM");
+
+  // client.write("Move: up");
+  // setTimeout(() => {
+  //   client.write("Move: up");
+  // }, 50);
+  // setInterval(() => {
+  //   client.write("Move: up");
+  // }, 50);
+
 });
+
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
